381f5aeaf86449a7a382ed606ad054015bb10e76
Vasyl Khreptak <87577206+VasylKhreptak@users.noreply.github.com>
2025-03-11 20:35:37 +0200

    feat: added autoinject;

---
+++ Assets/Scripts/Infrastructure/VContainer/Tools/AutoInject.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using VContainer;
+using VContainer.Unity;
+
+namespace Infrastructure.VContainer.Tools
+{
+    [DefaultExecutionOrder(-4999)]
+    public class AutoInject : MonoBehaviour
+    {
+        private IObjectResolver _resolver;
+
+        [Inject]
+        public void Construct(IObjectResolver resolver)
+        {
+            _resolver = resolver;
+            _injected = true;
+        }
+
+        private bool _injected;
+
+        private void Awake()
+        {
+            if (_injected)
+                return;
+
+            _resolver.InjectGameObject(gameObject);
+        }
+    }
+}
