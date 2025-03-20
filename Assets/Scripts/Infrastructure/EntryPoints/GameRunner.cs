d0165edb6d574c3dfa662d7fbae9cb4c93c964a8
Vasyl Khreptak <87577206+VasylKhreptak@users.noreply.github.com>
2025-03-20 21:24:09 +0200

    feat: added GameRunner;

---
+++ Assets/Scripts/Infrastructure/EntryPoints/GameRunner.cs
@@ -0,0 +1,26 @@
+﻿using Infrastructure.EntryPoints.Core;
+using UnityEngine;
+using VContainer.Unity;
+
+namespace Infrastructure.EntryPoints
+{
+    public class GameRunner : MonoBehaviour, IEntryPoint
+    {
+        #region MonoBehaviour
+
+        private void Start() => Enter();
+
+        #endregion
+
+        public void Enter()
+        {
+            if (FindObjectOfType<LifetimeScope>() != null)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            VContainerSettings.Instance.GetOrCreateRootLifetimeScopeInstance().Build();
+        }
+    }
+}
